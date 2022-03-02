82c98ea79111ea29f9718340730bea46e6353d8e
MysticMoz <ashikmoosa96@gmail.com>
2022-03-02 20:35:56 +0530

    Added ProductEditScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import OrderScreen from "./components/screens/OrderScreen";
 import UserListScreen from "./components/screens/UserListScreen";
 import UserEditScreen from "./components/screens/UserEditScreen";
 import ProductListScreen from "./components/screens/ProductListScreen";
+import ProductEditScreen from "./components/screens/ProductEditScreen";
 
 const App = () => {
   return (
@@ -38,6 +39,10 @@ const App = () => {
             <Route path="/admin/userlist" element={<UserListScreen />} />
             <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
             <Route path="/admin/productlist" element={<ProductListScreen />} />
+            <Route
+              path="/admin/product/:id/edit"
+              element={<ProductEditScreen />}
+            />
           </Routes>
         </Container>
       </main>
